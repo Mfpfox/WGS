5a2b03ab96267e4192ec5b346b62dafa0318af5d
Maria Palafox <mfpalafox@ucla.edu>
2024-06-14 04:29:11 -0400

    added 5-annotations

---
+++ 5-annotation/3-summarize/dtypes.txt.py
@@ -0,0 +1,12 @@
+Uploaded_variation       object
+Location                 object
+Allele                   object
+Gene                     object
+Feature                  object
+                          ...  
+HIGH_INF_POS             object
+MOTIF_SCORE_CHANGE       object
+TRANSCRIPTION_FACTORS    object
+chromosome               object
+chrCoordinate            object
+Length: 76, dtype: object
--- scripts/unique_first_col.sh
+++
@@ -1,3 +0,0 @@
-awk -F '\t' '{print $1}' raw_variants_CAD12.vcf | sort | uniq > unique_col1_raw_vcf_12.txt
-
-awk -F '\t' '{print $1}' raw_variants_CAD63.vcf | sort | uniq > unique_col1_raw_vcf_63.txt
